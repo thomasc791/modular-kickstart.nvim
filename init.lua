77f610b6cfa057044ae70bb99435056520af17cc
Thomas <t.o.c.cheng@student.tudelft.nl>
2024-07-11 14:42:37 +0200

    Changed nvim colorscheme

--- init.lua
+++ init.lua
@@ -114,6 +114,8 @@ vim.opt.showmode = false
 --  See `:help 'clipboard'`
 vim.opt.clipboard = 'unnamedplus'
 
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
 -- Enable break indent
 vim.opt.breakindent = true
 
--- lua/custom/plugins/custom.lua
+++ lua/custom/plugins/custom.lua
@@ -1,6 +1,18 @@
 local plugins = {
   'jose-elias-alvarez/null-ls.nvim',
-  'nvim-tree/nvim-tree.lua',
+  { 'nvim-tree/nvim-tree.lua', opts = {} },
+
+  'catppuccin/nvim',
+
+  {
+    'rmehri01/onenord.nvim',
+    name = 'onenord',
+    priority = 1000,
+    init = function()
+      vim.cmd.colorscheme 'onenord'
+      vim.cmd.hi 'Comment gui=none'
+    end,
+  },
 }
 
 return plugins
