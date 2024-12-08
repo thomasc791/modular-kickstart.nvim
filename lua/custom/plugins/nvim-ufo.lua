61072ffb800a0fd062756f766603b8fc1fb3b20d
Thomas <t.o.c.cheng@student.tudelft.nl>
2024-12-08 18:20:07 +0100

    Removed bars from nvim-ufo

--- lua/custom/plugins/nvim-ufo.lua
+++ lua/custom/plugins/nvim-ufo.lua
@@ -17,7 +17,7 @@ return {
     require('ufo').setup()
 
     vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
-    vim.o.foldcolumn = '1'
+    vim.o.foldcolumn = '0'
     vim.o.foldlevel = 99
     vim.o.foldlevelstart = 99
     vim.o.foldenable = true
